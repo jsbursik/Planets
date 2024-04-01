dd17c45f9befab755ee4a34abbe5c1aea33c5ce1
Jordan Bursik <jsbursik@gmail.com>
2024-04-01 16:43:52 -0500

    fetch fixed, also dumped data

--- planets.js
+++ planets.js
@@ -1,31 +1,33 @@
-const PLANETS = {
-  Mercury: "199",
-  Venus: "299",
-  Earth: "399",
-  Mars: "499",
-  Jupiter: "599",
-  Saturn: "699",
-  Uranus: "799",
-  Neptune: "899",
-};
+const fs = require("fs");
 
-let planetData = [];
+const PLANETS = [199, 299, 399, 499, 599, 699, 799, 899];
 
-const regex = /[XY] = ?(-?\d\.\d+E\+\d*)(?=.*\s*.*EOE)/g;
+const regex = /[XY] = ?-?\d\.\d+E\+\d*(?=.*\s*.*EOE)/g;
 
-async function fetchPlanets(p) {
-  const res = await fetch(
+let planetCoords = [];
+
+async function fetchPlanet(p) {
+  return fetch(
     `https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND=%27${p}%27&OBJ_DATA=%27YES%27&MAKE_EPHEM=%27YES%27&EPHEM_TYPE=%27VECTORS%27&CENTER=%27@Sun%27&START_TIME=%272024-03-27%27&STOP_TIME=%272024-03-28%27&VEC_TABLE=%271%27`
-  );
-  const data = await res.text();
-  if (data.includes("Unexpected Server Error")) {
-    fetchPlanets(p);
-  } else {
-    planetData.push(data);
-    console.log(p + " : " + regex.exec(data));
-  }
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Failed to fetch ${p}`);
+      }
+      return res.text();
+    })
+    .catch((err) => {
+      //console.error(err);
+      return fetchPlanet(p);
+    });
 }
 
-for (let i in PLANETS) {
-  fetchPlanets(PLANETS[i]);
+for (let i = 0; i < PLANETS.length; i++) {
+  fetchPlanet(PLANETS[i])
+    .then((data) => {
+      console.log(i + ":" + data.match(regex));
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
