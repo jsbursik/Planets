2e88d639f36c38864a37db08f2145285ff8d5078
Jordan Bursik <jsbursik@gmail.com>
2024-04-10 14:19:10 -0500

    working on scale

--- planets.js
+++ planets.js
@@ -1,7 +1,30 @@
 const PLANETS = [199, 299, 399, 499, 599, 699, 799, 899];
 const TIMEFRAME = getTimeframe();
 
+const HEIGHT = window.innerHeight;
+const WIDTH = window.innerWidth;
+
 let planetData = [];
+
+const dist_slider = document.getElementById("dist_slider");
+const rad_slider = document.getElementById("rad_slider");
+const dist_value = document.getElementById("dist_val");
+const rad_value = document.getElementById("rad_val");
+
+dist_value.textContent = dist_slider.value;
+rad_value.textContent = rad_slider.value;
+
+let dist_scale = 1000 * dist_slider.value;
+let rad_scale = 10 * rad_slider.value;
+
+dist_slider.addEventListener("input", () => {
+  dist_value.textContent = dist_slider.value;
+  dist_scale = 1000 * dist_slider.value;
+});
+rad_slider.addEventListener("input", () => {
+  rad_value.textContent = rad_slider.value;
+  rad_scale = 10 * rad_slider.value;
+});
 
 function getTimeframe() {
   const DATE = new Date();
@@ -19,8 +42,8 @@ function parseCoords(c) {
   // Example Coords: X = 1.339879730370736E+08 Y =-1.595979233568153E+08
   const r = /[XY] = ?(-?\d+.\d+E\+\d+)/;
   return {
-    x: parseFloat(c[0].match(r)[1] / 10000000),
-    y: parseFloat(c[1].match(r)[1] / 10000000),
+    x: parseFloat(c[0].match(r)[1]),
+    y: parseFloat(c[1].match(r)[1]),
   };
 }
 
@@ -43,12 +66,12 @@ for (let i = 0; i < PLANETS.length; i++) {
   fetchPlanet(PLANETS[i])
     .then((data) => {
       const nameRegex = /(?<=Target body name: )\w+/g;
+      const radRegex = /(?<=Vol\. Mean Radius \(km\)\s*=\s*)\d+/gi;
       const coordsRegex = /[XY] = ?(-?\d\.\d+E\+\d*)(?=.*\s*.*EOE)/g;
-      const name = data.match(nameRegex)[0];
-      const coords = data.match(coordsRegex);
       planetData.push({
-        planet: name,
-        coords: parseCoords(coords),
+        planet: data.match(nameRegex)[0],
+        radius: data.match(radRegex)[0],
+        coords: parseCoords(data.match(coordsRegex)),
       });
       console.log(planetData[planetData.length - 1]);
     })
@@ -58,14 +81,20 @@ for (let i = 0; i < PLANETS.length; i++) {
 }
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(WIDTH, HEIGHT);
 }
 
 function draw() {
   background(220);
-  translate(width / 2, height / 2);
-  fill(255);
+  translate(WIDTH / 2, HEIGHT / 2);
+  circle(0, 0, 695700 / rad_scale);
   for (let i = 0; i < planetData.length; i++) {
-    circle(planetData[i].coords.x, planetData[i].coords.y, 10);
+    let X = planetData[i].coords.x / dist_scale;
+    let Y = planetData[i].coords.y / dist_scale;
+    let R = planetData[i].radius / rad_scale;
+    fill(255);
+    circle(X, Y, R * 2);
+    fill(0);
+    text(planetData[i].planet, X, Y - (10 + R));
   }
 }
